b76171631d4c808e27092e20c95ecec7e04a644c
宇山拓夢 <jf1vax@gmail.com>
2016-02-08 13:31:23 +0900

    clean

--- create_json.js
+++ create_json.js
@@ -1,6 +1,28 @@
 var result = {tweets: 0, detail: []};
 var began_at = user_details.created_at.split(/-| /)[0] - 0;
 var Grailbird = {data: {}};
+var color = [
+  "#FFF",
+  "#B4F8FF",
+  "#4BEEFF",
+  "#00E6FF",
+  "#00FFEF",
+  "#00FFD1",
+  "#00FFA6",
+  "#00FF73",
+  "#00FF4D",
+  "#49FF00",
+  "#A6FF00",
+  "#DAFF00",
+  "#FCFF00",
+  "#FFE200",
+  "#FFC900",
+  "#FFB300",
+  "#FFA600",
+  "#FF9500",
+  "#FF6F00",
+  "#FF0000",
+];
 
 google.load("visualization", "1", {packages:["corechart"]});
 for(var i = tweet_index.length - 1; i >= 0; --i) {
