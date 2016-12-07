bad5ca1e6e4e3d4838a7c7ee0e5de0e47ffde54a
宇山拓夢 <sasurai.usagi3@gmail.com>
2016-12-07 19:24:15 +0900

    詳細も新しいコードに

--- js/detail.js
+++ js/detail.js
@@ -1,21 +1,22 @@
 function show_detail(){
-  var y = document.getElementById("input_year").value - 0 ;
-  var m = document.getElementById("input_month").value - 0;
-  var chart = new google.visualization.ColumnChart(document.getElementById('graph2'));
-  var data = google.visualization.arrayToDataTable([
+  let y = document.getElementById("input_year").value - 0 ;
+  let m = document.getElementById("input_month").value - 1;
+  let chart = new google.visualization.ColumnChart(document.getElementById('graph2'));
+  let data = google.visualization.arrayToDataTable([
     ['日', 'ツイート数'],
     ['', 0]
   ]);
-  var options = {
+  let options = {
     title: 'ツイート数遷移(' + y + '年' + m + '月)',
     legend: 'none',
     width: 1500,
     height: 750,
     vAxis:{maxValue: 50, gridlines: {count: 11}}
   };
-  result.set(y, m, 1);
-  for(; result.get().getMonth() + 1 == m; result.nextDate()){
-    data.addRows([[("0" + result.get().getDate()).slice(-2), result.at("date")]]);
+  let date = new Date(y, m, 1);
+  for(; m == date.getMonth(); date = new Date(date.valueOf() + 86400000)) {
+    let sum = result.where(x => new Date(x.created_at) >= date && new Date(x.created_at) < new Date(date.valueOf() + 86400000)).length;
+    data.addRows([[("0" + date.getDate()).slice(-2), sum]]);
   }
   chart.draw(data, options);
 }
