a4d2c576d9646059d7115f69afeed850add2f7e8
宇山拓夢 <sasurai.usagi3@gmail.com>
2016-12-07 22:44:13 +0900

    時間ごとのツイート数計測プログラム改良

--- docs/js/tweet_rhythm.js
+++ docs/js/tweet_rhythm.js
@@ -1,94 +1,57 @@
-function show_tweet_rhythm(){
-  var result2 = [];
-  var per_day_of_the_week = [];
-  var max_of_day_of_the_week = [];
-  var k = document.getElementById("input_k").value - 0;
+function show_tweet_rhythm() {
+  let max = 0, max2 = 0;
+  let k = document.getElementById("input_k").value - 0;
   var calendar = document.getElementById("calendar");
-  var color_graph = document.getElementById("color_graph");
 
   calendar.innerHTML = "";
-  color_graph.innerHTML = "";
-  var today = new Date();
-  result.reset();
-  per_day_of_the_week[7] = 0;
-  max_of_day_of_the_week[7] = 0;
-  for(;result.get() <= today; result.nextMinutes()) {
-    var date = result.get();
-    var tweeted_at = date.getFullYear() + "/" + ("0" + (date.getMonth() + 1)).slice(-2);
-    var tweets = result.at("date");
-    var h = result.get().getHours(), m = Math.floor(result.get().getMinutes() / result.rank), dtw = result.get().getDay();
-
-    if(result2[h] == null){
-      result2[h] = [];
-    }
-    if(result2[h][m] == null){
-      result2[h][m] = [];
-      result2[h][m][7] = 0;
-    }
-    if(result2[h][m][dtw] == null){
-      result2[h][m][dtw] = 0;
-    }
-    if(per_day_of_the_week[dtw] == null){
-      per_day_of_the_week[dtw] = 0;
-      max_of_day_of_the_week[dtw] = 0;
-    }
-    var delta = result.at("minutes");
-    result2[h][m][dtw] += delta;
-    result2[h][m][7] += delta;
-    per_day_of_the_week[dtw] += delta;
-    per_day_of_the_week[7] += delta;
-
-    if(max_of_day_of_the_week[dtw] < result2[h][m][dtw]){
-      max_of_day_of_the_week[dtw] = result2[h][m][dtw];
-    }
-    if(max_of_day_of_the_week[7] < result2[h][m][7]){
-      max_of_day_of_the_week[7] = result2[h][m][7];
+  for(let h = 0; h < 24; ++h) {
+    for(let m = 0; m < 60; m += 5) {
+      let sum = tweetData.where(x => x.created_at.getHours() === h && x.created_at.getMinutes() >= m && x.created_at.getMinutes() < m + 5).count();
+      max2 = Math.max(max2, sum);
+      for(let d = 0; d < 7; ++d) {
+        let sum = tweetData.where(x => x.created_at.getDay() === d && x.created_at.getHours() === h && x.created_at.getMinutes() >= m && x.created_at.getMinutes() < m + 5).count();
+        max = Math.max(max, sum);
+      }
     }
   }
-  console.log(result2);
-  console.log(max_of_day_of_the_week);
-  for(var h = 0; h < 24; ++h){
-    for(var m = 0; m < 12; ++m){
-      var tr = document.createElement("tr"), tr2 = document.createElement("tr");
-      var td, td2;
-      td = document.createElement("td");
-      td.textContent = ("0" + h).slice(-2) + ":" + ("0" + 5 * m).slice(-2) + "-" + ("0" + h).slice(-2) + ":" + ("0" + (5 * (m + 1) - 1)).slice(-2);
+  for(let h = 0; h < 24; ++h) {
+    for(let m = 0; m < 60; m += 5) {
+      let tr = document.createElement("tr");
+      let td = document.createElement("td");
+      td.textContent = ("0" + h).slice(-2) + ":" + ("0" + m).slice(-2) + "-" + ("0" + h).slice(-2) + ":" + ("0" + (m + 4)).slice(-2);
       tr.appendChild(td);
-      if(m == 0){
-        td2 = document.createElement("td");
-        td2.rowSpan = "12";
-        td2.style.backgroundColor = "#FFC";
-        td2.textContent = h;
-        tr2.appendChild(td2);
-      }
-      var total = 0;
-      for(var dtw = 0; dtw <= 7; ++dtw){
-        td = document.createElement("td");
-        td2 = document.createElement("td");
-        if(result2[h] == null || result2[h][m] == null || result2[h][m][dtw] == null){
-          td.textContent = 0;
+      for(let d = 0; d <= 7; ++d) {
+        let td = document.createElement("td");
+        let sum, level;
+        if(d === 7) {
+          sum = tweetData.where(x => x.created_at.getHours() === h && x.created_at.getMinutes() >= m && x.created_at.getMinutes() < m + 5).count();
+          level = sum / (max2 * k);
         } else {
-          td.textContent = result2[h][m][dtw];
-          tmp = (result2[h][m][dtw] / max_of_day_of_the_week[dtw] / k);
-          tmp = (tmp > 1)? 1 : tmp;
-          p = Math.floor(tmp * 20) - 1;
-          td2.style.backgroundColor = color[p];
+          sum = tweetData.where(x => x.created_at.getDay() === d && x.created_at.getHours() === h && x.created_at.getMinutes() >= m && x.created_at.getMinutes() < m + 5).count();
+          level = sum / (max * k);
         }
+        let index = Math.floor(((level >= 1) ? 0.999 : level) * 20);
+        td.textContent = sum;
+        td.style.backgroundColor = color[index];
         tr.appendChild(td);
-        tr2.appendChild(td2);
       }
-
       calendar.appendChild(tr);
-      color_graph.appendChild(tr2);
     }
   }
-  tr = document.createElement("tr");
-  td = document.createElement("td");
-  td.textContent = "合計値";
+
+  let tr = document.createElement("tr");
+  let td = document.createElement("td");
+  td.textContent = "合計"
   tr.appendChild(td);
-  for(var dtw = 0; dtw < 7; ++dtw){
-    td = document.createElement("td");
-    td.textContent = per_day_of_the_week[dtw];
+  for(let d = 0; d <= 7; ++d) {
+    let td = document.createElement("td");
+    let sum;
+    if(d === 7) {
+      sum = tweetData.count();
+    } else {
+      sum = tweetData.where(x => x.created_at.getDay() === d).count();
+    }
+    td.textContent = sum;
     tr.appendChild(td);
   }
   calendar.appendChild(tr);
